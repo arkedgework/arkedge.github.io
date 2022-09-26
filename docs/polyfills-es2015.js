d09341863bb27470606edd52c5dd243360051aad
arkedgework <arkedgework@gmail.com>
2022-09-26 14:43:01 +0000

    ak-l-1

--- docs/polyfills-es2015.js
+++ docs/polyfills-es2015.js
@@ -6491,8 +6491,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\github\arkedge\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! F:\github\arkedge\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /workspace/arkedge/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /workspace/arkedge/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -239,25 +239,25 @@ getUrl(i, title){
       clientView_1: ' We ensure excellence in service through understanding of our clients business and their unique requirements.',
       clientView_2: ' Due to our close relationships and constant interactions with clients,',
       clientView_3: ' we have developed solid understanding and insight in our focus industries that include.',
-      clientName: 'xyz Company'
+      clientName: 'test Company'
     },
     {
       clientView_1: ' Consectetur adipiscing elit. Aenean euismod bibendum laoreet.',
       clientView_2: ' Proin gravida dolor sit amet lacus accumsan et viverra justo commodo.',
       clientView_3: ' lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor.',
-      clientName: 'ABC Company'
+      clientName: 'test Company'
     },
     {
       clientView_1: ' We ensure excellence in service through understanding of our clients business and their unique requirements.',
       clientView_2: ' Due to our close relationships and constant interactions with clients,',
       clientView_3: ' we have developed solid understanding and insight in our focus industries that include.',
-      clientName: '123 Company'
+      clientName: 'test Company'
     },
     {
       clientView_1: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       clientView_2: ' Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo',
       clientView_3: ' Proin sodales pulvinar tempor.',
-      clientName: 'QWERTy'
+      clientName: 'test Company'
     },
 ];
 
