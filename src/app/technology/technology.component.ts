2407f91e5f276e5b5e9e4d325e8ef6b58013bc9d
bizexo <102503353+arkedgework@users.noreply.github.com>
2022-10-01 20:44:02 +0530

    ark-3

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -12,4 +12,56 @@ export class TechnologyComponent implements OnInit {
   ngOnInit() {
   }
 
+  slideConfig = {
+    rows: 2,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    nextArrow: `<div class='slick-prev slick-arrow'>  <i class="icon icon-large">
+    <svg focusable="false">
+      <use xlink:href="#icon_arrowLeft"></use>
+    </svg>
+  </i></div>`,
+    prevArrow: `<div class='slick-next slick-arrow'>  <i class="icon icon-large">
+    <svg focusable="false">
+      <use xlink:href="#icon_arrowRight"></use>
+    </svg>
+  </i></div>`,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
+  partner = [
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'},
+    {img: 'assets/images/client'}
+  ];
+
 }
